ebdd6fa90ebfd6fe240818d2835fc7595f3f81ff
uEriic <ueriic0@gmail.com>
2023-04-27 18:58:52 -0400

    Initial commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Banner</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Banner'
+}
+</script>
+<style scoped>
+#main-banner {
+    background-image: url('../assets/burger.jpg');
+    background-position: 0 -250px;
+    background-size: cover;
+    height: 600px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+#main-banner h1 {
+    color: white;
+    text-align: center;
+    font-size: 60px;
+    background-color: #222;
+    padding: 20px 40px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+    <nav>
+        <router-link to="/" id="logo-url">
+            <img :src="logo" :alt="alt" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/pedidos">Pedidos</router-link>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'Navbar',
+    props: {
+        logo: String,
+        alt: String
+    }
+}
+</script>
+<style scoped>
+    nav {
+        background-color: #222;
+        border-bottom: 4px solid #111;
+        padding: 15px 50px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        gap: 5rem;
+    }
+    nav #logo-url {
+        margin: auto;
+        margin-left: 0;
+    }
+    #logo {
+        width: 48px;
+        height: 48px;
+    }
+    nav a {
+        color: #fcba03;
+        text-decoration: none;
+        transition: .6s;
+        font-size: 24px;
+    }
+    nav a:hover {
+        color: white;
+        transform: scale(1.1);
+    }
+</style>
